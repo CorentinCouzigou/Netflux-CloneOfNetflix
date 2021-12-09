b194968ac01186875a40d55af3b5196b23f15fbc
Corentin Couzigou <80675299+CorentinCouzigou@users.noreply.github.com>
2021-12-09 14:00:44 +0100

    online CHECK

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Netflux - clone of Netflix
+[Lien vers le site](https://wizardly-pike-5eaae4.netlify.app/)
 
 React/Sass/MatérialUi/Axios/React-router-dom
 ![alt text](netfluxhome.png "image netflux")
--- src/components/Video.js
+++ src/components/Video.js
@@ -1,16 +1,17 @@
-import React, {useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
 import './Video.scss';
 import axios from 'axios';
-import TMDBKey from "../config/env";
 
+console.log(process.env)
+console.log(process.env.REACT_APP_TMDBKEY)
 
 function Video() {
     let { id } = useParams.apply();
-    const [youtubeKey, setYoutubeKey ] = useState("");
+    const [youtubeKey, setYoutubeKey] = useState("");
     useEffect(() => {
         async function fetchData() {
-            const response = await axios.get(`http://api.themoviedb.org/3/movie/${id}?api_key=${TMDBKey.clef}&append_to_response=videos`)
+            const response = await axios.get(`http://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDBKEY}&append_to_response=videos`)
             setYoutubeKey(response.data.videos.results[0].key);
             console.log("resultmovie", response.data.videos.results[0].key);
         }
